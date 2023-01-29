a209321a7b9a5bfb1359fcc6d65ef21d843b56c8
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-01-29 16:16:50 +0200

    Clean Up

---
+++ src/main/java/com/epam/donetc/restaurant/сontroller/command/post/SendEmail.java
@@ -0,0 +1,67 @@
+package com.epam.donetc.restaurant.сontroller.command.post;
+
+import com.epam.donetc.restaurant.сontroller.ICommand;
+
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+
+public class SendEmail implements ICommand {
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp)  {
+        try {
+            req.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String message = req.getParameter("message");
+        String username = "stanislavdonetc@gmail.com";
+        String password = "ehbejtlxthtjuosb";
+        String recipient = "santeh.vodavdom@gmail.com";
+
+        Properties props = new Properties();
+
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.from", "stanislavdonetc@gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "587");
+        props.setProperty("mail.debug", "true");
+
+        Session session = Session.getInstance(props, null);
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            msg.setRecipients(Message.RecipientType.TO, recipient);
+            msg.setSubject("Epam RESTAURANT");
+            msg.setSentDate(new Date());
+            msg.setText(name + email + message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        Transport transport = null;
+        try {
+            transport = session.getTransport("smtp");
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            transport.connect(username, password);
+            transport.sendMessage(msg, msg.getAllRecipients());
+            transport.close();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return req.getContextPath() + "/controller?command=info";
+
+    }
+
+
+}
+
--- target/restaurant-1.0-SNAPSHOT/WEB-INF/tags/greeting.tag
+++
@@ -1,6 +0,0 @@
-<%@ attribute name="user" type="com.epam.donetc.restaurant.database.entity.User" required="true" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<fmt:setLocale value="${sessionScope.lang}"/>
-<fmt:setBundle basename="messages"/>
-
-<fmt:message key="label.tag"/>  <font color="#228b22" size="2"><b>${user.login}</b></font>
